7ad768dfd89854c37575ac3777d722bddeff3c74
B.Etele <etele2004@gmail.com>
2025-11-23 10:39:49 +0100

    Create kaloriaszamlalo.py

---
+++ src/kaloriaszamlalo.py
@@ -0,0 +1,17 @@
+# kaloriaszamolo.py
+# Napi kalóriaszámítás több étkezésből
+
+from etkezes import Etkezes
+
+class KaloriaSzamolo:
+    def __init__(self):
+        self.etkezesek = []
+
+    def hozzaad_etkezes(self, etkezes: Etkezes):
+        self.etkezesek.append(etkezes)
+
+    def napi_ossz(self):
+        ossz = 0
+        for e in self.etkezesek:
+            ossz += e.ossz_kaloria()
+        return ossz

aed3bac5e54be694cd8114f9952a119ea4a22f63
B.Etele <etele2004@gmail.com>
2025-11-23 10:39:21 +0100

    Update etel.py

--- src/etel.py
+++ src/etel.py
@@ -1,1 +1,14 @@
+# etel.py
+# Egy egyszerű osztály ételek kezelésére
+
+class Etel:
+    def __init__(self, nev, kaloria, feherje, zsir, szenhidrat):
+        self.nev = nev
+        self.kaloria = kaloria
+        self.feherje = feherje
+        self.zsir = zsir
+        self.szenhidrat = szenhidrat
+
+    def info(self):
+        return f"{self.nev} - {self.kaloria} kcal"
 

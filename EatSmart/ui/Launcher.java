847ede99896d0209ff7deb5be4c4f72fb4c7015e
B.Etele <etele2004@gmail.com>
2025-12-08 16:16:44 +0100

    Add Launcher class to start EatSmart application

---
+++ EatSmart/ui/Launcher.java
@@ -0,0 +1,8 @@
+package com.eatsmart.ui;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Ez hívja meg az igazi alkalmazást
+        EatSmartApp.main(args);
+    }
+}
